a1ed262cef48be178c3b60c3034ce33fa5c11d43
hwangminghung <hwangminghung@gmail.com>
2024-03-24 15:40:42 +0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Nhập số hàng của ma trận:");
+        int rows = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Nhập số cột của ma trận:");
+        int columns = int.Parse(Console.ReadLine());
+
+        double[,] matrix = new double[rows, columns];
+
+        Console.WriteLine("Nhập các phần tử của ma trận:");
+
+        // Nhập giá trị cho từng phần tử của ma trận
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < columns; j++)
+            {
+                Console.WriteLine($"Nhập giá trị cho phần tử [{i}, {j}]:");
+                matrix[i, j] = double.Parse(Console.ReadLine());
+            }
+        }
+
+        double maxElement = matrix[0, 0];
+        int maxRow = 0;
+        int maxColumn = 0;
+
+        // Tìm phần tử lớn nhất trong ma trận
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < columns; j++)
+            {
+                if (matrix[i, j] > maxElement)
+                {
+                    maxElement = matrix[i, j];
+                    maxRow = i;
+                    maxColumn = j;
+                }
+            }
+        }
+
+        Console.WriteLine($"Phần tử lớn nhất trong ma trận có giá trị {maxElement} và tọa độ [{maxRow}, {maxColumn}].");
+    }
+}
